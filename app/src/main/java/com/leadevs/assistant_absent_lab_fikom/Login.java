064f9d81aa49487855f3e4661486ee911b4f7b8e
trisnandar <trisnandar172@gmail.com>
2020-06-19 07:00:28 +0700

    Ndarr

---
+++ app/src/main/java/com/leadevs/assistant_absent_lab_fikom/Login.java
@@ -0,0 +1,31 @@
+package com.leadevs.assistant_absent_lab_fikom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login = findViewById(R.id.btnLogin);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
+    }
+
+}
